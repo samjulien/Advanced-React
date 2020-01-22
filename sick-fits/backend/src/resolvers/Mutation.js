5d551e620e471730d4132dcb58ef42a890fbef81
Sam Julien <sam.julien@auth0.com>
2020-01-21 17:31:41 -0800

    Add update and delete items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -10,6 +10,28 @@ const Mutations = {
       info
     );
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item(
+      { where },
+      `
+      {id title}
+    `
+    );
+    // TODO check if own it
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+import { ALL_ITEMS_QUERY } from "./Items";
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // manually update cache on client so it matches the server
+    // read cache for items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    // filter deleted item out
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    );
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button
+            onClick={() => {
+              if (confirm("Are you sure?")) {
+                deleteItem();
+              }
+            }}
+          >
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+export default DeleteItem;
