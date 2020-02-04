729b7abb3c693787fded261025b0235691b42609
Sam Julien <spjulien@gmail.com>
2020-02-04 13:17:27 -0800

    Add signup and me query

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -2,11 +2,21 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 const server = createServer();
 
 server.express.use(cookieParser());
 // TODO use Express middleware to populate current user
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
